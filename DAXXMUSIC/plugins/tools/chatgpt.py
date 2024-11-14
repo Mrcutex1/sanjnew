26986b013d5b2f23595dcf2e6e58f83bdc652b7d
Mr cute x <139148328+Mrcutex1@users.noreply.github.com>
2024-11-14 07:50:46 +0530

    Update chatgpt.py

--- DAXXMUSIC/plugins/tools/chatgpt.py
+++ DAXXMUSIC/plugins/tools/chatgpt.py
@@ -5,7 +5,7 @@ from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 from MukeshAPI import api
 
-@app.on_message(filters.command(["chatgpt", "ai", "ask", "", "yt"], prefixes=[".", "a", "j","J", "A", "", "/"]))
+@app.on_message(filters.command(["chatgpt", "ai", "ask", "", "iri"], prefixes=["s","S", "a", "j","J", "A", "", "/"]))
 async def chat_gpt(bot, message):
     try:
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
