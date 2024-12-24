61cc927155e547bb0887696ad2b6056c4b6269ea
Mr cute x <139148328+Mrcutex1@users.noreply.github.com>
2024-12-24 17:36:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -69,8 +69,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/blazebots_support")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/blazebots_support")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/blaze_bots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/blaze_bots_support")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
